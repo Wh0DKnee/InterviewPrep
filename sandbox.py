1c900f1e0ed7bc026a2c180e7d0be79a04dea0cc
Simon Reuter <s.reuter1@stud.tu-darmstadt.de>
2023-01-24 15:28:33 +0100

    more solutions

--- sandbox.py
+++ sandbox.py
@@ -1,34 +1,20 @@
-from typing import List
+def minimumPerimeter(radius):
+    apples = 0
+
+    upper_n, lower_n = 2 * radius - 1, radius
+    # everything except corners and axis points
+    apples += 8 * ((((upper_n ** 2) + upper_n) / 2) - (((lower_n ** 2) + lower_n) / 2))
+    # corners and axis points:
+    apples += 12 * radius
+
+    return int(apples)
 
 
-class NumArray:
+prev = 0
+l = []
+for i in range(1, 11):
+    app = minimumPerimeter(i) + prev
+    prev = app
+    l.append(app)
 
-    def __init__(self, nums: List[int]):
-        self.sums = [0] * len(nums)
-        self.sums[0] = nums[0]
-
-        for i in range(1, len(nums)):
-            self.sums[i] = nums[i] + self.sums[i-1]
-
-    def update(self, index: int, val: int) -> None:
-        old = self.sums[index]
-        self.sums[index] = val
-        if index > 0:
-            self.sums[index] += self.sums[index - 1]
-        diff = self.sums[index] - old
-
-        for i in range(index+1, len(self.sums)):
-            self.sums[index] += diff
-
-    def sumRange(self, left: int, right: int) -> int:
-        print(self.sums)
-        if left == 0:
-            return self.sums[right]
-        return self.sums[right] - self.sums[left - 1]
-
-
-# Your NumArray object will be instantiated and called as such:
-numArray = NumArray([1, 3, 5])
-numArray.sumRange(0, 2)
-numArray.update(1, 2)
-numArray.sumRange(0, 2)
+print(l)
